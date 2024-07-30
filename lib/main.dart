9f9b921aac45b17780548241416ec87f1c94270c
teamkotlin <muhammadramzan6254@gmail.com>
2024-07-30 18:31:52 +0500

    flutter animated added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:techhype/list_english_words.dart';
+import 'package:techhype/temp.dart';
 import 'package:techhype/ui/voice_recognition_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: VoiceRecognitionScreen(),
+      home: FlutterAnimateExample(),
 
     );
   }
